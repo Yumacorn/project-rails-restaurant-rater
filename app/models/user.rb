adc941f1101b67f354b4f28f4d20607c4e3fc452
Yumacorn <ReachRyanMui@gmail.com>
2020-12-06 12:46:12 -0500

    added validation for state on database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :restaurants, through: :reviews
-        
+    
+    US_STATES = %w(AL AK AS AZ AR CA CO CT DE DC FM FL GA GU HI ID IL IN IA KS KY LA ME MH MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND MP OH OK OR PW PA PR RI SC SD TN TX UT VT VI VA WA WV WI WY)
+
     # Validations
+    before_validation { self.state = self.state.upcase }
+
     validates :name, presence: true
     validates :username, presence: true, uniqueness: true
     validates :age, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 99} 
     validates :city, presence: true
-    validates :state, presence: true    
+    validates :state, presence: true, inclusion: { in: US_STATES,
+        message: "%{value} is not a valid US state" }
 end
