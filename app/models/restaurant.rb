a018dbea1fe4b809fbfdc2600b4f17a1f78483f8
Yumacorn <ReachRyanMui@gmail.com>
2020-12-06 13:36:13 -0500

    created global variable US_STATES for validation, added validations for Restaurant creation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,12 @@ class Restaurant < ApplicationRecord
     has_many :users, through: :reviews
 
     # Validations
-    validates :name, presence: true
+    before_validation { self.state = self.state.upcase }
+
+    validates :name, presence: true, uniqueness:true
     validates :city, presence: true
-    validates :state, presence: true
+    validates :state, presence: true, inclusion: { in: US_STATES,
+        message: "%{value} is not a valid US state" }
+    validates :dine_in, presence: true, :inclusion => {:in => [true, false]}
+    validates :take_out, presence: true, :inclusion => {:in => [true, false], message: "%{value} must be true or false"}
 end
