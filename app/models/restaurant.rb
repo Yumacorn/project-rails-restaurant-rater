a78cab9b85e1792db01a1373a6b2b0b4c57920f4
Yumacorn <ReachRyanMui@gmail.com>
2020-12-06 12:00:55 -0500

    adding basic validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,11 @@
 class Restaurant < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
+
+    # Validations
+    validates :name, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :age, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :restaurants, through: :reviews
+        
+    # Validations
+    validates :name, presence: true
+    validates :username, presence: true, uniqueness: true
+
 end
