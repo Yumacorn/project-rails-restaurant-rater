03ab04e5ec37827693260492b9973d721f61c7d7
Yumacorn <ReachRyanMui@gmail.com>
2020-12-06 20:48:59 -0500

    finished index view for restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,6 +9,6 @@ class Restaurant < ApplicationRecord
     validates :city, presence: true
     validates :state, presence: true, inclusion: { in: US_STATES,
         message: "%{value} is not a valid US state" }
-    validates :dine_in, presence: true, :inclusion => {:in => [true, false]}
-    validates :take_out, presence: true, :inclusion => {:in => [true, false], message: "%{value} must be true or false"}
+    validates :dine_in, inclusion: { in: [true, false] , message: "%{value} must be true or false"}
+    validates :take_out, inclusion: { in: [true, false] }
 end
