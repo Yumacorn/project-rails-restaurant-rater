b4ab047b8443fdb87a099cce6e575d3ab8c23d54
Yumacorn <ReachRyanMui@gmail.com>
2020-12-07 00:24:48 -0500

    added Review view routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,33 @@ class ReviewsController < ApplicationController
     def index
         @reviews = Review.all
     end
+
+    def show
+        @review = Review.find(params[:id])
+    end
+
+    def new
+        @review = Review.new
+    end
+
+    def create
+        @review = Review.create(review_params)
+        binding.pry
+        redirect_to review_path(@review)
+    end
+
+    def edit
+        @review = Review.find(params[:id])
+    end
+
+    def update
+        @review = Review.find(params[:id])
+        @review.update(review_params)
+        redirect_to review_path(@review)
+    end
+
+    private
+    def review_params
+        params.require(:review).permit(:user_id, :restaurant_id, :rating, :dining_method, :comment)
+    end
 end
