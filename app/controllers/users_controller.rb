4eb5a44aa9dd264f27d5ac563d2950e285bbd2d0
Yumacorn <ReachRyanMui@gmail.com>
2020-12-06 21:54:44 -0500

    adding navigation between User views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
 
     def edit
         @user = User.find(params[:id])
-        redirect_to user_path(@user)
+        # redirect_to user_path(@user)
     end
 
     private
