c01420774ef3897014f8b70b95bc2a6c9e1ef4fe
Yumacorn <ReachRyanMui@gmail.com>
2020-12-08 00:35:00 -0500

    wired up Login and Signup buttons to hit views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: 'application#home'
   get '/home', to: 'application#home'
 
+  get '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   resources :reviews
   resources :restaurants
   resources :users
