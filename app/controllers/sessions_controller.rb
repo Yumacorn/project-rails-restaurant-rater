df7223a9ce2ad5bcb92d966b9b7b6c4e21a9821e
Yumacorn <ReachRyanMui@gmail.com>
2020-12-08 19:18:34 -0500

    before we attempt OmniAuth and OmniAuth Google gems

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:username])
     return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
+    # render 'sessions/welcome'
   end
   
   def destroy
