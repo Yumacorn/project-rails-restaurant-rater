9cae078bf95854010489748be0a7be991da5ec15
Yumacorn <ReachRyanMui@gmail.com>
2020-12-19 11:28:14 -0500

    adding email column to user db table

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,9 @@ class User < ApplicationRecord
 
     validates :name, presence: true
     validates :username, presence: true, uniqueness: true
-    validates :age, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 99}
-    validates :city, presence: true
-    validates :state, presence: true, inclusion: { in: US_STATES, message: "%{value} is not a valid US state" }
+    validates :age, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 99}, allow_nil: true
+    # validates :city, presence: true
+    validates :state, inclusion: { in: US_STATES, message: "%{value} is not a valid US state" }, allow_blank: true
 
     def self.from_omniauth(auth)
       #if github, this is how to pull username request.env['omniauth.auth']['info']['nickname']
@@ -23,12 +23,17 @@ class User < ApplicationRecord
       binding.pry
       if auth.provider == "github"
         where(username: auth.info.nickname).first_or_initialize do |user|
+          user.email = auth.info.email
+          user.password = SecureRandom.hex(10)
+          user.name = auth.info.name
           user.username = auth.info.nickname
         end
       elsif auth.provider == "google_oauth2"
         where(username: auth.info.email).first_or_initialize do |user|
-          user.username = auth.info.email
+          user.email = auth.info.email
+          user.password = SecureRandom.hex(10)
           user.name = auth.info.name
+          user.username = auth.info.email
         end
       end
     end   
