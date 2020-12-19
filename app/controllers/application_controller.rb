ab8d986300a559de79da70e234e6d20c11bce551
Yumacorn <ReachRyanMui@gmail.com>
2020-12-19 14:05:14 -0500

    hide/display login/signup options if not logge din

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
 
-    def authentication_required
-        if !logged_in
-            redirect_to login_path
+    private
+        def authentication_required
+            if !logged_in?
+                redirect_to login_path
+            end
         end
-    end
 
-    def logged_in
-        session[:user_id]
-    end
+        def logged_in?
+            !!session[:user_id]
+        end
 end
