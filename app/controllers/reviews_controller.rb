b3037c8ad741628b78415fc17666b0623f2eb263
Yumacorn <ReachRyanMui@gmail.com>
2020-12-20 00:51:26 -0500

    editable reviews by user and deletable

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,11 +12,15 @@ class ReviewsController < ApplicationController
     end
 
     def new
-        @review = Review.new
+        if params[:user_id] && @user = User.find_by_id(params[:user_id])
+            @review = @user.reviews.build
+        else
+            @review = Review.new
+        end
     end
 
     def create
-        @review = Review.create(review_params)
+        @review = User.find(current_user).reviews.build(review_params)
         if @review.save
             redirect_to review_path(@review)
         else
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
 
   resources :users, only: [:new, :create, :index, :show, :edit, :update] do
-    resources :reviews, only: [:new, :create, :index]
+    resources :reviews
   end
   resources :restaurants do
     resources :reviews, only: [:new, :create, :index]
