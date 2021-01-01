155837becc05e1ed11b0c80f08e849a1ec82e82c
Yumacorn <ReachRyanMui@gmail.com>
2021-01-01 12:48:28 -0500

    commit tree c69a4a7f0863e2be329599b43a5a2d6e278f0a27

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -3,12 +3,9 @@ module Filterable
   
     module ClassMethods
       def filter(filtering_params, object)
-        binding.pry
         results = self.where(nil)
         if filtering_params[object].present?
-          binding.pry
             filtering_params[object].each do |key, value|
-              binding.pry
                 # [2] pry(Restaurant)> filtering_params[object]
                 # => <ActionController::Parameters {"filter_by_state"=>"NY"} permitted: false>
                 results = results.public_send("#{key}", value) if value.present?
@@ -18,10 +15,8 @@ module Filterable
                 # => "FL"
             end
         elsif filtering_params["search"].present?
-          binding.pry
           # results = Restaurant.filter_by_restaurant(filtering_params["search"])
           filtering_params["search"].each do |key, value|
-            binding.pry
             results = results.public_send("#{key}", value)
           end
         end
