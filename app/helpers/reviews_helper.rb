3d1c3b3ec159b8529820b911ada0567a74d14ff2
Yumacorn <ReachRyanMui@gmail.com>
2021-04-29 00:18:00 -0400

    added visual star ratings to each review

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,9 @@
 module ReviewsHelper
+    def num_stars_review(score)
+        tally = ''
+        score.times do |i|
+            tally += image_tag('star-icon.jpg',size: "15")
+        end
+        tally.html_safe
+    end
 end
